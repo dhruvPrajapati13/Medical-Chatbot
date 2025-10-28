5471a9b64b9cd0e2213a4a5e875802c484ff8670
dhruvPrajapati13 <dp.dhruvprajapati.13@gmail.com>
2025-10-28 17:31:35 +0530

    folders added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='GenAI Project',
+    version='0.0.0',
+    author='Dhruv Prajapati' ,
+    author_email= 'dp.dhruvprajapati.13@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
